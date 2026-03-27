1862d8a8670ea311bff74c20758bd67db069bf58
Armandas <ec25470@qmul.ac.uk>
2026-03-27 11:26:43 +0000

    changed for loop to 100

--- HW.java
+++ HW.java
@@ -3,7 +3,7 @@ class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-		for(int i = 0; i < 10; i++)
+		for(int i = 0; i < 100; i++)
 		{
 			System.out.println(i);
 		}
